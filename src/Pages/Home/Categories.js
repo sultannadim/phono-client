7c9f588db906c27cece2fdb6fc567c5535a91746
sultan5218 <sultan5218@gmail.com>
2022-11-24 01:25:41 +0600

    categories part complete

---
+++ src/Pages/Home/Categories.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <section className="pt-5 pb-lg-5 mt-sm-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h1 className="mb-sm-4 mb-3">All Categories For Resell</h1>
+          </div>
+          {categories.map((category) => (
+            <div className="col-lg-4 col-md-6" key={category._id}>
+              <Card className="mb-lg-0 mb-sm-4 mb-3">
+                <Card.Img
+                  className="cat-img"
+                  variant="top"
+                  src={category.img}
+                />
+                <Card.Body>
+                  <Card.Title>{category.categoryName}</Card.Title>
+                  <Card.Text>{category.title}</Card.Text>
+                  <Button variant="primary" className="bg-dark border-dark">
+                    View All
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Categories;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "./Banner";
+import Categories from "./Categories";
 import Extra from "./Extra";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Categories></Categories>
       <Extra></Extra>
     </div>
   );
