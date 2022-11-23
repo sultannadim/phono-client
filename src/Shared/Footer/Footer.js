e2ca3e5df95709aea55cae1115d93aa39198c02b
sultan5218 <sultan5218@gmail.com>
2022-11-23 23:32:37 +0600

    footer part complete

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <footer className="py-3 footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="footer-content">
+              <p className="mb-0 text-center">© 2022 Phono Powered by React</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
