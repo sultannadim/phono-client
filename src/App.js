83cc6a537bee8768cefae606a6e6edbe7ce5f101
sultan5218 <sultan5218@gmail.com>
2022-11-24 00:01:51 +0600

    banner part complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import "./App.css";
+import "./media.css";
 
 function App() {
   return (
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <section className="banner">
+      <div className="container">
+        <div className="row justify-content-end">
+          <div className="col-lg-7">
+            <div className="banner-content">
+              <h1>Buy and resell your phone</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
