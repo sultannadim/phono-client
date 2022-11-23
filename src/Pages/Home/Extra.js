acc2cfdd1911eac6139f55138c1b43c06a29e67c
sultan5218 <sultan5218@gmail.com>
2022-11-24 00:19:48 +0600

    extra part complete

---
+++ src/Pages/Home/Extra.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Extra = () => {
+  return (
+    <section className="py-5 my-sm-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="extra-img-box mb-lg-0 mb-5">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0104/6917/9492/files/bg-1.png?v=1613702304"
+                className="img-fluid"
+                alt="mobile"
+              />
+            </div>
+          </div>
+          <div className="col-lg-6 d-lg-flex flex-lg-column justify-content-lg-center">
+            <h2 className="mb-4">42 FRONT CAMERA FOR PERFECT SHOT</h2>
+            <p>
+              42 MP Camera Mobile Phones! At present, smartphone users are like
+              to use big Camera with Quality features. So, all the Mobile
+              companies try to enter the big camera in their upcoming
+              smartphones. Now, many rumors are available about the 42MP Camera
+              mobile phones.
+            </p>
+            <p>
+              Some HTC, LG & Nokia Upcoming Mobile Phones 2018 comes 42 Mega
+              Pixel Camera. But, it is not an official announcement. Many big
+              technology websites are publishing rumors 42 MP Camera smartphones
+              in 2018 – 2019. In this content, we will talk about the Upcoming
+              42 MP Camera Mobile Phones in 2018. So, staying with us and follow
+              the below content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Extra;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import Extra from "./Extra";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Extra></Extra>
     </div>
   );
 };
